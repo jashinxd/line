d94ce74cfd7cb119439a0087c082f7533e18d7d7
Jason Shin <jasonshin98@gmail.com>
2016-02-23 13:27:58 -0500

    half works

--- draw.c
+++ draw.c
@@ -7,17 +7,17 @@
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  printf("y0: %d\n", y0);
-  printf("y1: %d\n", y1);
-  printf("x0: %d\n", x0);
-  printf("x1: %d\n", x1);
-  
-  float m = (y1 - y0)/(x1 - x0);
-  printf("slope: %f\n", m);
-  int x = x0; int y = y0;
-  if (m > 0 && m < 1) {
-    int A = y1 - y0; int B = -(x1 - x0); int d = 2 * A + B;
-    while (x <= x1) {
+  float m = (float)(y1 - y0)/(float)(x1 - x0);
+  int x = x0; int y = y0; int A = y1 - y0; int B = -(x1 - x0);
+  int goalX = x1; int goalY = y1;
+  if (x0 > x1) {
+    x = x1; y = y1; A = y0 - y1; B = -(x0 - x1);
+    goalX = x0; goalY = y0;
+  }
+  printf("slope: %f", m);
+  if (m > 0 && m <= 1) {
+    int d = 2 * A + B;
+    while (x <= goalX) {
       plot(s, c, x, y);
       if (d > 0) {
 	y += 1;
@@ -25,6 +25,17 @@ void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
       }
       x += 1;
       d += 2 * A;
+    }
+  } else if (m > 1) {
+    int d = A + 2 * B;
+    while (y <= goalY) {
+      plot(s, c, x, y);
+      if (d < 0) {
+	x += 1;
+	d += 2 * A;
+      }
+      y += 1;
+      d += 2 * B;
     }
   }
 }
